f669c5ef17f142ab7566877b75d2b2db3aafc67d
Dmitry White <dmitry.b.white@gmail.com>
2020-01-18 13:40:54 +0300

    Add calc util

---
+++ js/utils/calc.js
@@ -0,0 +1,14 @@
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const getRandomFloat = (min, max) => {
+  return min + Math.random() * (max + 1 - min);
+}
+
+export {
+  getRandomInt,
+  getRandomFloat,
+}
